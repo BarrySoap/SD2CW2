46c70e5da77c2b1329af8276d37fc3910ccc84a6
Wilkie-Sullivan <40208762@live.napier.ac.uk>
2016-10-28 14:11:48 +0100

    Made basic validators for the guest class (may be changed).

--- Guests.cs
+++ Guests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace CW2
 {
@@ -15,26 +16,76 @@ namespace CW2
 
         public string GuestFirstName
         {
-            get { return guestFirstName; }
-            set { guestFirstName = value; }
+            get
+            {
+                return guestFirstName;
+            }
+            set
+            {
+                if (guestFirstName.Length == 0)
+                {
+                    MessageBox.Show("First name field cannot be blank!");
+                } else
+                {
+                    guestFirstName = value;
+                }
+            }
         }
 
         public string GuestSecondName
         {
-            get { return guestSecondName; }
-            set { guestSecondName = value; }
+            get
+            {
+                return guestSecondName;
+            }
+            set
+            {
+                if (guestSecondName.Length == 0)
+                {
+                    MessageBox.Show("Second name field cannot be blank!");
+                } else
+                {
+                    guestSecondName = value;
+                }
+            }
         }
 
         public string GuestPassNumber
         {
-            get { return guestPassNumber; }
-            set { guestPassNumber = value; }
+            get
+            {
+                return guestPassNumber;
+            }
+            set
+            {
+                if (guestPassNumber.Length > 0 && guestPassNumber.Length < 11)
+                {
+                    guestPassNumber = value;
+                } else
+                {
+                    MessageBox.Show("Invalid value! Passport value must be between 1-10 characters!");
+                }
+                
+            }
         }
 
         public int GuestAge
         {
-            get { return guestAge; }
-            set { guestAge = value; }
+            get
+            {
+                return guestAge;
+            }
+            set
+            {
+                if (guestAge > 0 && guestAge < 102)
+                {
+                    guestAge = value;
+                } else
+                {
+                    MessageBox.Show("Invalid age! You must be between 0-102 years old to enjoy this holiday!");
+                }
+                
+            }
         }
     }
 }
