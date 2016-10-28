6ac60354f5da79e4ac9da61e3c833f9815347b7d
Wilkie-Sullivan <40208762@live.napier.ac.uk>
2016-10-28 11:44:34 +0100

    Initial Commit - Booking variables and properties.

---
+++ Booking.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Author: Glenn Wilkie-Sullivan (40208762)
+ * Class Purpose: Contains values/properties for bookings
+ * Date last modified: 28/10/2016
+ */
+
+namespace CW2
+{
+    class Booking
+    {
+        private DateTime arrivalDate = DateTime.MinValue;           // Values are initialised as
+        private DateTime departureDate = DateTime.MinValue;         // "01 January 0001"
+        private double refNumber = 1;
+
+        public DateTime ArrivalDate
+        {
+            get { return arrivalDate; }
+            set { arrivalDate = value; }
+        }
+
+        public DateTime DepartureDate
+        {
+            get { return departureDate; }
+            set { departureDate = value; }
+        }
+
+        public double RefNumber
+        {
+            get { return refNumber; }
+            set { refNumber = value + 1; }
+        }
+            
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CW2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddCBG_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnExtras_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnInvoice_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDelCustomer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnDelBooking_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAmendCBG_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
