1c5f8a3dc6612bf83d4f0f29269c4ccce4de7cba
Wilkie-Sullivan <40208762@live.napier.ac.uk>
2016-10-28 11:52:44 +0100

    Customer class shell created, initialised variables/properties.

---
+++ Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW2
+{
+    class Customer
+    {
+        private string firstName;
+        private string secondName;
+        private double custRefNumber = 1;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string SecondName
+        {
+            get { return secondName; }
+            set { secondName = value; }
+        }
+
+        public double CustRefNumber
+        {
+            get { return custRefNumber; }
+            set { custRefNumber = value + 1; }
+        }
+    }
+}
