592f54e82f5ca3717ebb12daaf90205e3f1c9a47
Wilkie-Sullivan <40208762@live.napier.ac.uk>
2016-11-16 15:14:23 +0000

    Added validators to customer/booking addition button, added functionality to guest input.

--- CBG.xaml.cs
+++ CBG.xaml.cs
@@ -19,6 +19,10 @@ namespace CW2
     /// </summary>
     public partial class CBG : Window
     {
+        Guests guest1 = new Guests();
+        Booking book1 = new Booking();
+        Customer cust1 = new Customer();
+
         public CBG()
         {
             InitializeComponent();
@@ -28,7 +32,7 @@ namespace CW2
         public void hideAdditions()
         {
             lblCBG.Visibility = Visibility.Hidden;
-            btnaddCust.Visibility = Visibility.Hidden;
+            btnaddCBG.Visibility = Visibility.Hidden;
             lblProperty1.Visibility = Visibility.Hidden;
             lblProperty2.Visibility = Visibility.Hidden;
             lblProperty3.Visibility = Visibility.Hidden;
@@ -39,8 +43,7 @@ namespace CW2
 
         private void btnAddCust_Click(object sender, RoutedEventArgs e)
         {
-            Customer cust1 = new Customer();
-            btnaddCust.Visibility = Visibility.Visible;
+            btnaddCBG.Visibility = Visibility.Visible;
             lblProperty1.Visibility = Visibility.Visible;
             lblProperty2.Visibility = Visibility.Visible;
             lblProperty3.Visibility = Visibility.Visible;
@@ -56,14 +59,13 @@ namespace CW2
 
         private void btnAddBooking_Click(object sender, RoutedEventArgs e)
         {
-            Booking book1 = new Booking();
-            btnaddCust.Visibility = Visibility.Visible;
+            btnaddCBG.Visibility = Visibility.Visible;
             lblProperty1.Visibility = Visibility.Visible;
             lblProperty2.Visibility = Visibility.Visible;
             lblCBG.Visibility = Visibility.Visible;
             lblCBG.Content = "Add a Booking:";
-            lblProperty1.Content = "Arrival Date:";
-            lblProperty2.Content = "Departure Date:";
+            lblProperty1.Content = "Arrival Date (dd/mm/yyyy):";
+            lblProperty2.Content = "Departure Date (dd/mm/yyyy):";
             lblProperty3.Visibility = Visibility.Hidden;
             txtCBG1.Visibility = Visibility.Visible;
             txtCBG2.Visibility = Visibility.Visible;
@@ -75,9 +77,34 @@ namespace CW2
             
         }
 
-        private void btnaddCust_Click(object sender, RoutedEventArgs e)
+        private void btnaddCBG_Click_1(object sender, RoutedEventArgs e)
         {
+            if (lblCBG.Content.ToString() == "Add a Customer:")
+            {
+                if (txtCBG1.Text != "" && txtCBG2.Text != "" && txtCBG3.Text != "")
+                {
+                    cust1.CustomerFirstName = txtCBG1.Text;
+                    cust1.CustomerSecondName = txtCBG2.Text;
+                    cust1.CustomerAddress = txtCBG3.Text;
+                } else
+                {
+                    MessageBox.Show("The above fields must be valid/not blank!");
+                }
+            } 
 
+            if (lblCBG.Content.ToString() == "Add a Booking:")
+            {
+                DateTime d;
+
+                if (DateTime.TryParse(txtCBG1.Text, out d) && DateTime.TryParse(txtCBG2.Text, out d))
+                {
+                    book1.ArrivalDate = DateTime.Parse(txtCBG1.Text);
+                    book1.DepartureDate = DateTime.Parse(txtCBG2.Text);
+                } else
+                {
+                    MessageBox.Show("The arrival date and departure date boxes must be valid dates!");
+                }
+            }
         }
     }
 }
