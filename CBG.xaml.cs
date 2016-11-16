6cad69b85a1be3052499a5ff67c087f7f80ff744
Wilkie-Sullivan <40208762@live.napier.ac.uk>
2016-11-16 16:03:15 +0000

    Fixed validation issues, added guest input and functionality.

--- CBG.xaml.cs
+++ CBG.xaml.cs
@@ -34,9 +34,11 @@ namespace CW2
             lblCBG.Visibility = Visibility.Hidden;
             btnaddCBG.Visibility = Visibility.Hidden;
             lblProperty1.Visibility = Visibility.Hidden;
+            lblProperty_5.Visibility = Visibility.Hidden;
             lblProperty2.Visibility = Visibility.Hidden;
             lblProperty3.Visibility = Visibility.Hidden;
             txtCBG1.Visibility = Visibility.Hidden;
+            txtCBG1_5.Visibility = Visibility.Hidden;
             txtCBG2.Visibility = Visibility.Hidden;
             txtCBG3.Visibility = Visibility.Hidden;
         }
@@ -45,43 +47,61 @@ namespace CW2
         {
             btnaddCBG.Visibility = Visibility.Visible;
             lblProperty1.Visibility = Visibility.Visible;
+            lblProperty_5.Visibility = Visibility.Visible;
             lblProperty2.Visibility = Visibility.Visible;
-            lblProperty3.Visibility = Visibility.Visible;
             lblCBG.Visibility = Visibility.Visible;
             lblCBG.Content = "Add a Customer:";
             lblProperty1.Content = "First Name:";
-            lblProperty2.Content = "Last Name:";
-            lblProperty3.Content = "Address:";
+            lblProperty_5.Content = "Last Name:";
+            lblProperty2.Content = "Address:";
             txtCBG1.Visibility = Visibility.Visible;
+            txtCBG1_5.Visibility = Visibility.Visible;
             txtCBG2.Visibility = Visibility.Visible;
-            txtCBG3.Visibility = Visibility.Visible;
         }
 
         private void btnAddBooking_Click(object sender, RoutedEventArgs e)
         {
+            btnaddCBG.Content = "Add Booking";
             btnaddCBG.Visibility = Visibility.Visible;
-            lblProperty1.Visibility = Visibility.Visible;
             lblProperty2.Visibility = Visibility.Visible;
+            lblProperty3.Visibility = Visibility.Visible;
             lblCBG.Visibility = Visibility.Visible;
             lblCBG.Content = "Add a Booking:";
-            lblProperty1.Content = "Arrival Date (dd/mm/yyyy):";
-            lblProperty2.Content = "Departure Date (dd/mm/yyyy):";
-            lblProperty3.Visibility = Visibility.Hidden;
-            txtCBG1.Visibility = Visibility.Visible;
+            lblProperty2.Content = "Arrival Date (dd/mm/yyyy):";
+            lblProperty3.Content = "Departure Date (dd/mm/yyyy):";
+            lblProperty1.Visibility = Visibility.Hidden;
+            lblProperty_5.Visibility = Visibility.Hidden;
             txtCBG2.Visibility = Visibility.Visible;
-            txtCBG3.Visibility = Visibility.Hidden;
+            txtCBG3.Visibility = Visibility.Visible;
+            txtCBG1.Visibility = Visibility.Hidden;
+            txtCBG1_5.Visibility = Visibility.Hidden;
         }
 
         private void btnAddGuest_Click(object sender, RoutedEventArgs e)
         {
-            
+            btnaddCBG.Content = "Add Guest";
+            btnaddCBG.Visibility = Visibility.Visible;
+            lblProperty1.Visibility = Visibility.Visible;
+            lblProperty_5.Visibility = Visibility.Visible;
+            lblProperty2.Visibility = Visibility.Visible;
+            lblProperty3.Visibility = Visibility.Visible;
+            lblCBG.Visibility = Visibility.Visible;
+            lblCBG.Content = "Add a Guest:";
+            lblProperty1.Content = "First Name:";
+            lblProperty_5.Content = "Last Name:";
+            lblProperty2.Content = "Passport Number:";
+            lblProperty3.Content = "Age:";
+            txtCBG1.Visibility = Visibility.Visible;
+            txtCBG1_5.Visibility = Visibility.Visible;
+            txtCBG2.Visibility = Visibility.Visible;
+            txtCBG3.Visibility = Visibility.Visible;
         }
 
         private void btnaddCBG_Click_1(object sender, RoutedEventArgs e)
         {
             if (lblCBG.Content.ToString() == "Add a Customer:")
             {
-                if (txtCBG1.Text != "" && txtCBG2.Text != "" && txtCBG3.Text != "")
+                if (txtCBG1.Text != "" && txtCBG1_5.Text != "" && txtCBG2.Text != "")
                 {
                     cust1.CustomerFirstName = txtCBG1.Text;
                     cust1.CustomerSecondName = txtCBG2.Text;
@@ -96,10 +116,10 @@ namespace CW2
             {
                 DateTime d;
 
-                if (DateTime.TryParse(txtCBG1.Text, out d) && DateTime.TryParse(txtCBG2.Text, out d))
+                if (DateTime.TryParse(txtCBG2.Text, out d) && DateTime.TryParse(txtCBG3.Text, out d))
                 {
-                    book1.ArrivalDate = DateTime.Parse(txtCBG1.Text);
-                    book1.DepartureDate = DateTime.Parse(txtCBG2.Text);
+                    book1.ArrivalDate = DateTime.Parse(txtCBG2.Text);
+                    book1.DepartureDate = DateTime.Parse(txtCBG3.Text);
                 } else
                 {
                     MessageBox.Show("The arrival date and departure date boxes must be valid dates!");
