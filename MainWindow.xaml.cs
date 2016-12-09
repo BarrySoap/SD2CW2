2629e7fa6cbb8c819cac01ca6be32e40116dd78e
Glenn Sullivan <Glenn Sullivan>
2016-12-09 23:42:57 +0000

    Added comments to the MainWindow GUI.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,11 +13,13 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+/* Author: Glenn Wilkie-Sullivan (40208762)
+ * Class Purpose: Represents the main hub of the program, allows access to each GUI.
+ * Date last modified: 09/12/2016
+ */
+
 namespace CW2
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary> 
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -27,37 +29,37 @@ namespace CW2
 
         private void btnAddCBG_Click(object sender, RoutedEventArgs e)
         {
-            CBG cuboguest = new CBG();
-            cuboguest.Show();
+            CBG cuboguest = new CBG();                                      // Allows access to the window for creating
+            cuboguest.Show();                                               // customers, bookings and guests.
         }
 
         private void btnExtras_Click(object sender, RoutedEventArgs e)
         {
-            ExtrasPrompt extra = new ExtrasPrompt();
-            extra.Show();
+            ExtrasPrompt extra = new ExtrasPrompt();                        // Allows access to the window for adding
+            extra.Show();                                                   // extras to a booking.
         }
 
         private void btnInvoice_Click(object sender, RoutedEventArgs e)
         {
-            Invoice inv = new Invoice();
+            Invoice inv = new Invoice();                                    // Allows access to the invoice for a booking.
             inv.Show();
         }
 
         private void btnDelCustomer_Click(object sender, RoutedEventArgs e)
         {
-            DeleteCustomer del1 = new DeleteCustomer();
+            DeleteCustomer del1 = new DeleteCustomer();                     // Allows access to the window for deleting a customer.
             del1.Show();
         }
 
         private void btnDelBooking_Click(object sender, RoutedEventArgs e)
         {
-            DeleteBooking delbook1 = new DeleteBooking();
+            DeleteBooking delbook1 = new DeleteBooking();                   // Allows access to the window for deleting a booking.
             delbook1.Show();
         }
 
         private void btnAmendCBG_Click(object sender, RoutedEventArgs e)
         {
-            AmendCBG amend1 = new AmendCBG();
+            AmendCBG amend1 = new AmendCBG();                               // Allows access to the window for amending customers, bookings and guests.
             amend1.Show();
         }
     }
