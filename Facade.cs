3e61b982a61c7b3cb76817760c04abd3e148a63e
Glenn Sullivan <Glenn Sullivan>
2016-12-09 23:28:38 +0000

    Added comments to the facade class.

--- Facade.cs
+++ Facade.cs
@@ -4,13 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* Author: Glenn Wilkie-Sullivan (40208762)
+ * Class Purpose: Contains logic for a facade.
+ * Date last modified: 09/12/2016
+ */
+
 namespace CW2
 {
     class Facade
     {
-        private DeleteBooking delbook;
-        private DeleteCustomer delcust;
-        private DeletionHelp delHelp;
+        private DeleteBooking delbook;              // Initialise objects for deleting a booking,
+        private DeleteCustomer delcust;             // deleting a customer,
+        private DeletionHelp delHelp;               // and the helper window.
 
         public Facade()
         {
@@ -19,16 +24,16 @@ namespace CW2
             delHelp = new DeletionHelp();
         }
 
-        public void deleteBooking(double temp)
+        public void deleteBooking(double temp)                  // Temp will be replaced with the respective reference number.
         {
-            delHelp.checkBookingValidity(temp);
-            delHelp.deleteBooking(delbook.bookingPath, temp);
+            delHelp.checkBookingValidity(temp);                 // Use the method in the helper window for checking if a booking exists.
+            delHelp.deleteBooking(delbook.bookingPath, temp);   // If it exists, delete it when prompted.
         }
 
         public void deleteCustomer(double temp)
         {
-            delHelp.checkCustomerValidity(temp);
-            delHelp.deleteCust(delcust.customerPath, temp);
+            delHelp.checkCustomerValidity(temp);                // Use the method in the helper window for checking if a customer exists.
+            delHelp.deleteCust(delcust.customerPath, temp);     // If it exists, delete it when prompted.
         }
     }
 }
